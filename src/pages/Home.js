b2553434a7aef439c65ca2734c26fb5b202450f6
anas599 <anas599@yahoo.com>
2023-06-26 12:00:00 +0300

    remove comments

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,9 @@
 import { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import TutoriesGallery from '../components/home/TutoriesGallery';
-// import { useCurrentUserQuery } from '../api/usersData';
-// import Login from './Login';
-import {
-  fetchTutories,
-} from '../redux/tutories/tutoriesSlice';
-
-/* TODO: tutories should be fetched from API */
+import { fetchTutories } from '../redux/tutories/tutoriesSlice';
 
 export default function Home() {
-  // import error and status once the API is deployed
   const dispatch = useDispatch();
 
   const mediaQuery = window.matchMedia('(min-width: 1024px)');
@@ -32,18 +25,22 @@ export default function Home() {
 
   useEffect(() => {
     /* create media match event listener for slider responsiveness */
-    const onVwChange = ({ matches }) => { setIsComputerWidth(matches); };
+    const onVwChange = ({ matches }) => {
+      setIsComputerWidth(matches);
+    };
     mediaQuery.addEventListener('change', onVwChange);
-    return () => { mediaQuery.removeEventListener('change', onVwChange); };
+    return () => {
+      mediaQuery.removeEventListener('change', onVwChange);
+    };
   }, [dispatch]);
 
   /* Determine if user has reached max permitted scroll in slider */
   useEffect(() => {
     if (tutoriesStatus !== 'fulfilled') return;
-    let pagesScrolled = ((translated * -1) / amountToTranslate.current);
+    let pagesScrolled = (translated * -1) / amountToTranslate.current;
     // when translated is 0 and amount to translate is also 0 pages scrolled isNaN
     pagesScrolled = Number.isNaN(pagesScrolled) ? 0 : pagesScrolled;
-    setHasReachedMaxScroll((amountScrollPages - 1) === pagesScrolled);
+    setHasReachedMaxScroll(amountScrollPages - 1 === pagesScrolled);
   }, [dispatch, translated, amountScrollPages, tutoriesStatus]);
 
   useEffect(() => {
@@ -63,7 +60,10 @@ export default function Home() {
       <div>
         <div className="all:center-text mb-1">
           <h1 className="title">Tutories Subjects</h1>
-          <p className="subtitle">Please select a subject about which you would like to receive tutories</p>
+          <p className="subtitle">
+            Please select a subject about which you would like to receive
+            tutories
+          </p>
         </div>
         <div className="separator mb-2" />
         <TutoriesGallery
@@ -75,28 +75,28 @@ export default function Home() {
         />
       </div>
       {isComputerWidth && (
-      <>
-        <button
-          type="button"
-          className="arrow arrow--left"
-          onClick={() => { translateLeft(amountToTranslate.current); }}
-          disabled={translated === 0}
-        >
-          <span className="material-symbols-outlined">
-            play_arrow
-          </span>
-        </button>
-        <button
-          type="button"
-          className="arrow arrow--right"
-          onClick={() => { translateRight(amountToTranslate.current); }}
-          disabled={hasReachedMaxScroll}
-        >
-          <span className="material-symbols-outlined">
-            play_arrow
-          </span>
-        </button>
-      </>
+        <>
+          <button
+            type="button"
+            className="arrow arrow--left"
+            onClick={() => {
+              translateLeft(amountToTranslate.current);
+            }}
+            disabled={translated === 0}
+          >
+            <span className="material-symbols-outlined">play_arrow</span>
+          </button>
+          <button
+            type="button"
+            className="arrow arrow--right"
+            onClick={() => {
+              translateRight(amountToTranslate.current);
+            }}
+            disabled={hasReachedMaxScroll}
+          >
+            <span className="material-symbols-outlined">play_arrow</span>
+          </button>
+        </>
       )}
     </section>
   );
